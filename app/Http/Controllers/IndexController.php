6dee4883133bfa72df1629a681ca7ddec7137fab
hyb804141892 <804141892@qq.com>
2018-09-05 14:11:13 +0800

    init

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/12
+ * Time: 11:34
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class IndexController
+{
+
+
+}
